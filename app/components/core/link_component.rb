2fc4608601a73b191b6262e504527ac9875e10f7
Matias Szeinfeld <mattszeindev@gmail.com>
2023-12-21 20:36:46 -0300

    add sidebar, refactoring components and add cookies lib

--- app/components/core/link_component.rb
+++ app/components/core/link_component.rb
@@ -1,29 +1,44 @@
 class Core::LinkComponent < ViewComponent::Base
   include ActionView::Helpers::UrlHelper
-  attr_accessor :as_button, :theme, :size, :name, :url, :options
+  attr_accessor :style, :theme, :size, :name, :url, :options
 
-  DEFAULT = {as_button: true, theme: :primary, size: :md}.freeze
+  STYLE = {default: :default,
+           as_button: :as_button,
+           no_style: :no_style}.freeze
+  DEFAULT = {style: :as_button, theme: :primary, size: :md}.freeze
 
-  def initialize(name, url, options = {})
+  def initialize(name: nil, url: nil, options: {}, &block)
     custom_style = options&.delete(:custom_style) || {}
     style_merged = DEFAULT.merge(custom_style)
-    @as_button = style_merged[:as_button]
+
+    @style = style_merged[:style]
     @theme = style_merged[:theme]
     @size = style_merged[:size]
     @name = name
     @url = url
+    @options = options
   end
 
   def html_class
-    case as_button
-    when true
+    case style
+    when :as_button
       Core::Form::ButtonComponent.new(nil, nil, {theme: theme, size: size}).html_class
-    else
+    when :default
       "inline-flex items-center justify-center border border-transparent rounded-md font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2"
     end
   end
 
-  erb_template <<-ERB
-    <%= link_to(name, url, class: html_class) %>
+  def render_content
+    if content
+      link_to(url, options) do
+        content
+      end
+    else
+      link_to(name, url, class: html_class)
+    end
+  end
+
+  erb_template <<~ERB
+    <%= render_content %>
   ERB
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/components", under: "components"
+pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "js-cookie", to: "https://ga.jspm.io/npm:js-cookie@3.0.5/dist/js.cookie.mjs"
